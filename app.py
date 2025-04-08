5ce206c05c439ae3f49423a052fdaedcfbc044fc
Wolfy1011404 <120730911+Wolfy1011404@users.noreply.github.com>
2025-04-08 11:41:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,4 @@ def get_data():
   return jsonify(data)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
